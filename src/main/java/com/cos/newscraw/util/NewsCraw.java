adbb1b39a244b73a823f520c558bd33745ee002b
ssar <ssar@example.com>
2021-09-28 16:16:21 +0900

    try/catch문 넣기

--- src/main/java/com/cos/newscraw/util/NewsCraw.java
+++ src/main/java/com/cos/newscraw/util/NewsCraw.java
@@ -20,31 +20,37 @@ public class NewsCraw {
 		RestTemplate rt = new RestTemplate();
 		List<News> newsList = new ArrayList<>();
 
-		for (int i = 1; i < 6; i++) {
-			String aid = String.format("%010d", aidNum);
-			String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=022&aid=" + aid;
-			String html = rt.getForObject(url, String.class);
+		try {
+			for (int i = 1; i < 6; i++) {
+				String aid = String.format("%010d", aidNum);
+				String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=022&aid=" + aid;
+				String html = rt.getForObject(url, String.class);
 
-			Document doc = Jsoup.parse(html);
+				Document doc = Jsoup.parse(html);
 
-			Element companyElement = doc.selectFirst(".article_header .press_logo img");
-			Element titleElement = doc.selectFirst("#articleTitle");
-			Element createdAtElement = doc.selectFirst(".t11");
-			String company = companyElement.attr("alt");
-			String title = titleElement.text();
-			String createdAt =createdAtElement.text();
+				Element companyElement = doc.selectFirst(".article_header .press_logo img");
+				Element titleElement = doc.selectFirst("#articleTitle");
+				Element createdAtElement = doc.selectFirst(".t11");
+				String company = companyElement.attr("alt");
+				String title = titleElement.text();
+				String createdAt =createdAtElement.text();
 
 
-			News news = News.builder()
-					.company(company)
-					.title(title)
-					.createdAt(createdAt)
-					.build();
-					
-			newsList.add(news);
+				News news = News.builder()
+						.company(company)
+						.title(title)
+						.createdAt(createdAt)
+						.build();
+						
+				newsList.add(news);
 
-			aidNum++;
+				aidNum++;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
 		return newsList;
 	}
 }
