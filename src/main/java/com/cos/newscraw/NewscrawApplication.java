54cac21d91ec88d25abe0bbddf9130f2e9974d45
ssar <ssar@example.com>
2021-09-28 13:51:49 +0900

    개발환경 설정

---
+++ src/main/java/com/cos/newscraw/NewscrawApplication.java
@@ -0,0 +1,13 @@
+package com.cos.newscraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewscrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/newscraw/NewscrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.newscraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
